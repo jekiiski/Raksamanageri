ac273a6efa6bf6361fa118fea3e8e7cd862a534d
appD-eino <eino.kiiski@gmail.com>
2014-09-04 12:16:43 +0300

    päivityksiä

---
+++ Raksamanageri/src/main/java/fi/raksamanageri/domain/main.java
@@ -0,0 +1,11 @@
+
+package fi.raksamanageri.domain;
+
+public class main {
+    
+    public static void main(String[] args) {
+        // tänne tulee koodia
+        System.out.println("Hello World");
+    }
+    
+}
